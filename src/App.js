7acefcc62a1821e5094283fc77a4397798109b9d
Alwallo <alvgonzalezb@gmail.com>
2023-04-12 22:08:54 -0600

    baner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componentes/Header.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header></Header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Holis soy Alvaro y estoy aprendiendo Git con Douglas
---
+++ src/componentes/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header>
+            <img src="/img/baner.png" className='estilo' alt="baner" />
+        </header>
+    );
+};
+
+export default Header;
